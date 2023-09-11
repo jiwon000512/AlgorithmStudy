c00caaa784a540aa72e59fbd273a99ec46a9f6e3
Jiwon <55117734+jiwon000512@users.noreply.github.com>
2023-09-11 19:09:55 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.cc
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string in;
+
+void Solve()
+{
+    int ans = 0;
+    string num = "";
+    bool minus = false;
+
+    for (int i = 0; i < static_cast<int>(in.size()); i++)
+    {
+        if (in[i] == '-')
+        {
+            if (minus)
+            {
+                ans -= stoi(num);
+            }
+            else
+            {
+                ans += stoi(num);
+                minus = true;
+            }
+            num = "";
+        }
+        else if (in[i] == '+')
+        {
+            if (minus)
+            {
+                ans -= stoi(num);
+            }
+            else
+            {
+                ans += stoi(num);
+            }
+            num = "";
+        }
+        else
+        {
+            num += in[i];
+        }
+    }
+
+    if (minus)
+    {
+        ans -= stoi(num);
+    }
+    else
+    {
+        ans += stoi(num);
+    }
+
+    cout << ans << '\n';
+}
+
+int main()
+{
+    cin >> in;
+    Solve();
+}
