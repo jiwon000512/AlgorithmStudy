590ea367c2cce79c9bf665f99adf9b23c66acef8
Jiwon <55117734+jiwon000512@users.noreply.github.com>
2025-01-09 22:22:49 +0900

    Update 가장 긴 바이토닉 부분 수열.cc

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.cc
@@ -4,51 +4,62 @@
 using namespace std;
 
 vector<int> v;
+vector<vector<int>> memo;
+
+int dp(int index, bool plus);
 
 int main()
 {
     int n;
     cin >> n;
     v.resize(n);
+    memo.resize(2, vector<int>(n, 0));
     for (int i = 0; i < n; i++)
     {
         cin >> v[i];
     }
 
-    vector<int> inc(n, 1); // 증가하는 부분 수열의 길이
-    vector<int> dec(n, 1); // 감소하는 부분 수열의 길이
-
-    // 증가하는 부분 수열 계산
-    for (int i = 1; i < n; i++)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (v[i] > v[j] && inc[i] < inc[j] + 1)
-            {
-                inc[i] = inc[j] + 1;
-            }
-        }
-    }
-
-    // 감소하는 부분 수열 계산
-    for (int i = n - 2; i >= 0; i--)
-    {
-        for (int j = n - 1; j > i; j--)
-        {
-            if (v[i] > v[j] && dec[i] < dec[j] + 1)
-            {
-                dec[i] = dec[j] + 1;
-            }
-        }
-    }
-
-    // 가장 긴 바이토닉 수열의 길이 계산
     int ans = 0;
     for (int i = 0; i < n; i++)
     {
-        ans = max(ans, inc[i] + dec[i] - 1);
+        ans = max(ans, dp(i, true));
+        ans = max(ans, dp(i, false));
     }
 
     cout << ans << endl;
     return 0;
+}
+
+int dp(int index, bool plus)
+{
+    if (index == v.size() - 1)
+    {
+        memo[0][index] = 1;
+        return memo[1][index] = 1;
+    }
+
+    if (memo[plus][index] != 0)
+    {
+        return memo[plus][index];
+    }
+
+    for (int i = index + 1; i < v.size(); i++)
+    {
+        if (v[index] == v[i])
+            continue;
+
+        bool nextIsPlus = v[index] <= v[i];
+        memo[nextIsPlus][index] = max(memo[nextIsPlus][index], dp(i, nextIsPlus) + 1);
+        if (nextIsPlus)
+        {
+            memo[nextIsPlus][index] = max(memo[nextIsPlus][index], dp(i, false) + 1);
+        }
+    }
+
+    if (memo[plus][index] == 0)
+    {
+        memo[plus][index] = 1;
+    }
+
+    return memo[plus][index];
 }
